4de9835968c4f5b779ae70e31afa0e4c695b050e
greencoder001 <64889695+greencoder001@users.noreply.github.com>
2020-10-10 12:43:55 +0200

    1.0.0

---
+++ src/main.js
@@ -0,0 +1,53 @@
+class Operation {
+  constructor () {
+    this.handlers = {}
+
+    this.states = ['pending', 'successfully', 'failed']
+    this.state = this.states[0]
+  }
+
+  dispatch (e, value = null) {
+    if (e === 'success') {
+      this.state = this.states[1]
+    } else if (e === 'fail') {
+      this.state = this.states[2]
+    }
+
+    for (const handler of this.handlers[e] || []) {
+      handler(value, e)
+    }
+  }
+
+  fail (err = new Error('Operation failed')) {
+    this.dispatch('fail', err)
+  }
+
+  success (value = '') {
+    this.dispatch('success', value)
+  }
+
+  on (e, handler) {
+    if (typeof this.handlers[e] !== 'object') {
+      this.handlers[e] = []
+    }
+
+    this.handlers[e].push(handler)
+  }
+}
+
+const waitFor = (operation, callback) => {
+  operation.on('success', callback)
+}
+
+const isSuccess = operation => {
+  return operation.state === operation.states[1]
+}
+
+const waitfor = waitFor
+
+if (window) {
+  window.waitFor = waitFor
+  window.waitfor = waitfor
+  window.Operation = Operation
+  window.isSuccess = isSuccess
+}
