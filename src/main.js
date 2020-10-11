87e4380e023bfc954fd5097ce0b67d18026ae914
greencoder001 <64889695+greencoder001@users.noreply.github.com>
2020-10-11 16:17:39 +0200

    1.0.1 - Multiple values

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ class Operation {
     this.on('fail', callback)
   }
 
-  dispatch (e, value = null) {
+  dispatch (e, ...values) {
     if (e === 'success') {
       this.state = this.states[1]
     } else if (e === 'fail') {
@@ -19,16 +19,16 @@ class Operation {
     }
 
     for (const handler of this.handlers[e] || []) {
-      handler(value, e)
+      handler(...values)
     }
   }
 
-  fail (err = new Error('Operation failed')) {
-    this.dispatch('fail', err)
+  fail (...values) {
+    this.dispatch('fail', ...values)
   }
 
-  success (value = '') {
-    this.dispatch('success', value)
+  success (...values) {
+    this.dispatch('success', ...values)
   }
 
   on (e, handler) {
